2374a837355f801569915f869a4f446c3376b737
peter-emad99 <peteremad95@gmail.com>
2024-04-25 20:30:11 +0200

    setup the app

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="text-red-600 text-xl font-bold text-center">Task Manager</h1>
+  </div>
+  <main></main>
+</template>
+
+<style scoped></style>
